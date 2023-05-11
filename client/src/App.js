1e76154b539d4cc8aa7452c134d83944ac51530e
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2023-05-11 17:08:29 +1000

    Renamed pages slightly

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import createList from './pages/createList';
+import CreateList from './pages/CreateList';
 import Home from './pages/Home';
-import searchUser from './pages/searchUser';
+import SearchUser from './pages/SearchUser';
 import User from './pages/User';
-import viewList from './pages/viewList';
+import ViewList from './pages/ViewList';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -16,9 +16,30 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
           <Routes>
-            <Route exact path="/" component={Home} />
+            <Route 
+              path="/" 
+              element={<Home />}
+            />
+            <Route 
+              path="/CreateList" 
+              element={<CreateList />}
+            />
+            <Route 
+              path="/SearchUser" 
+              element={<SearchUser />}
+            />
+            <Route 
+              path="User"
+              element={<User />}
+            />
+            <Route 
+              path="ViewList"
+              element={<ViewList />}
+            />
           </Routes>
+        </div>
       </Router>
     </ApolloProvider>
   );
